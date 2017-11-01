12e316e3963767dfaceaebd127bb8733ee9368e8
wangzs <wangzs@smartdot.com>
2017-11-01 10:24:14 +0800

    初版

---
+++ oa/oa/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  oa
+//
+//  Created by 王战胜 on 2017/10/31.
+//  Copyright © 2017年 gocomtech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ oa/oa/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  oa
+//
+//  Created by 王战胜 on 2017/10/31.
+//  Copyright © 2017年 gocomtech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
